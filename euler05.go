2b4f2b3b4902e78a29bd7a6bf4490e77ff73f98d
Stuart Fletcher <stuartf@subpop.com>
2013-10-10 07:47:48 -0700

    go fmt problem 5

--- euler05.go
+++ euler05.go
@@ -2,7 +2,7 @@
 // Problem 5
 // 2520 is the smallest number that can be divided by each of the
 // numbers from 1 to 10 without any remainder.
-// 
+//
 // What is the smallest positive number that is evenly divisible by all
 // of the numbers from 1 to 20?
 package main
@@ -44,4 +44,5 @@ func try1() int {
 	}
 	return 0
 }
+
 // 232792560
