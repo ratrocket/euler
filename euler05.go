932e3317118aa516e0b25de5adead089480e0962
Stuart Fletcher <stuartf@subpop.com>
2013-10-09 17:01:14 -0700

    Pretty much brute forcing #5.
    Haven't checked solution yet.

--- euler05.go
+++ euler05.go
@@ -9,9 +9,39 @@ package main
 
 import "fmt"
 
+/*
+A number divisible by 20 is already divisible by
+1, 2, 4, 5, 10
+So only look at multiples of 20 & don't test for divisibility by those
+factors.
+The remaining factors:
+3: only every third multiple of 20 is divisble by 3 (60, 120, ...) so
+   check multiples of 60.
+6: hmmm, same advice as for 3 (w/o thinking too much)
+7: multiples of 420? (least common for 60 & 7)
+8: multiples of 840? (this might be going off the rails)
+*/
 func main() {
+	fmt.Println(try1())
 }
 
 func try1() int {
+	increment := 420
+	// leave out 15 because it divides 420
+	for i := increment; ; i += increment {
+		if i%8 == 0 &&
+			i%9 == 0 &&
+			i%11 == 0 &&
+			i%12 == 0 &&
+			i%13 == 0 &&
+			i%14 == 0 &&
+			i%16 == 0 &&
+			i%17 == 0 &&
+			i%18 == 0 &&
+			i%19 == 0 {
+			return i
+		}
+	}
 	return 0
 }
+// 232792560
