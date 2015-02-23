539c7f615a5e6694443f1c043f920da6873f9043
Stuart Fletcher <stuart.fletcher@gmail.com>
2015-02-23 14:57:37 -0800

    Problems 7 and 8.

--- euler07.go
+++ euler07.go
@@ -4,13 +4,47 @@
 // can see that the 6th prime is 13.
 //
 // What is the 10 001st prime number?
+//
+// A: 104743
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
+	n := 10001
+	primes := make([]int, 1)
+	for i := 1; len(primes) < 10001+1; i++ {
+		if prime(i) {
+			primes = append(primes, i)
+		}
+	}
+	fmt.Printf("prime %d: %d\n", n, primes[n])
 }
 
 func try1() int {
 	return 0
+}
+
+func prime(n int) bool {
+	if n == 1 { return false }
+	if n == 2 { return true }
+	// some ad hoc "optimization"
+	if n > 2 && n&1 == 0 { return false }
+	if n > 3 && n%3 == 0 { return false }
+	if n > 5 && n%5 == 0 { return false }
+	if n > 7 && n%7 == 0 { return false }
+	if n > 11 && n%11 == 0 { return false }
+	if n > 13 && n%13 == 0 { return false }
+	if n > 17 && n%17 == 0 { return false }
+
+	var top int = int(math.Ceil(math.Sqrt(float64(n))))
+	for i := int(2); i <= top; i++ {
+		if n % i == 0 {
+			return false
+		}
+	}
+	return true
 }
